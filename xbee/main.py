1cd693a1332bde998ffe0925710f002ed3390d53
Cameron <candbb08@gmail.com>
2020-03-11 10:20:04 -0400

    xbee update 3-11-2020

--- xbee/main.py
+++ xbee/main.py
@@ -1,7 +1,8 @@
 import time
 from xbee import relay
 
-MSG_ACK = "OK"
+#MSG_ACK = "OK"
+device = "WSM"
 
 INTERFACES = {
     relay.SERIAL: "Serial Port",
@@ -15,23 +16,46 @@ INTERFACES = {
 # TODO: Read WSM info and populate it into a string to send. - Cameron
 # TODO: Error handle. - Stevan
 
-while True:
-    # relay.receive() -> Receives the message from the client
-    relay_frame = relay.receive()
-
-    # Stay in loop until an actual message is received
-    while relay_frame is None:
+def sending():
+    while True:
+        # relay.receive() -> Receives the message from the client
         relay_frame = relay.receive()
-        time.sleep(0.25)
 
-    # Send acknowledgment message back to client
-    relay.send(relay.BLUETOOTH, MSG_ACK)
+        # Stay in loop until an actual message is received
+        while relay_frame is None:
+            relay_frame = relay.receive()
+            time.sleep(0.25)
 
-    # Don't change this line. This is how it knows who to send the message to.
-    sender = relay_frame["sender"]
+        if relay_frame is not None:
+        # Send acknowledgment message back to client
+            relay.send(relay.BLUETOOTH, "OK")
 
-    # Alter 'message' to send the desired payload
-    message = relay_frame["message"].decode("utf-8")
+            # Don't change this line. This is how it knows who to send the message to.
+            sender = relay_frame["sender"]
 
-    # Send the message and start reading relay frames again.
-    relay.send(sender, "%s" % message)
+            # Alter 'message' to send the desired payload
+            file = open("example.txt", "r")
+            lines = file.readlines()
+            message = ""
+            for line in lines:
+                message = message + line
+                message = message + "@@@"
+
+            # Send the message and start reading relay frames again.
+            relay.send(sender, message)
+
+        else:
+            # No connection between XBee and Android
+            print("no connection")
+            time.sleep(30)
+
+def timeout():
+    # After 5 times of trying or 2 minutes, XBee will break
+    timeout = time.time() + 60 * 1  # 5 minutes from now
+    while True:
+        test = 0
+        if test == 5 or time.time() > timeout:
+            break
+        test = (test + 1)
+        
+sending()
